76c3404c723cc86abd28456fc0d87b25df0225f4
Logan Lembke <logan.lembke@mines.sdsmt.edu>
2016-11-30 16:29:16 -0700

    remove unused import

--- VoronoiDemo/VoronoiDemo.cs
+++ VoronoiDemo/VoronoiDemo.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using VoronoiLib;
